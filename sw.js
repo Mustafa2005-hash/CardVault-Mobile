648650df4cec1c39ad42f140d9f9217c575aa1ba
Mustafa2005-hash <74418107+Mustafa2005-hash@users.noreply.github.com>
2025-08-22 14:33:08 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-const PRECACHE = 'cv-cache-local-v1';
+const PRECACHE = 'cv-cache-local-v2';
 const PRECACHE_URLS = ['./','./index.html','./manifest.json'];
-
-self.addEventListener('install', (e)=>{
-  e.waitUntil((async()=>{
-    const cache = await caches.open(PRECACHE);
-    await cache.addAll(PRECACHE_URLS);
-  })());
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e)=>{
-  e.waitUntil((async()=>{
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k=>k!==PRECACHE).map(k=>caches.delete(k)));
-    await self.clients.claim();
-  })());
-});
-
-self.addEventListener('fetch', (e)=>{
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin){
-    e.respondWith(caches.match(e.request, {ignoreSearch:true}).then(r => r || fetch(e.request)));
-  }
-});
+self.addEventListener('install', (e)=>{ e.waitUntil(caches.open(PRECACHE).then(c=>c.addAll(PRECACHE_URLS))); self.skipWaiting(); });
+self.addEventListener('activate', (e)=>{ e.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.filter(k=>k!==PRECACHE).map(k=>caches.delete(k))); await self.clients.claim(); })()); });
+self.addEventListener('fetch', (e)=>{ const url=new URL(e.request.url); if(url.origin===location.origin){ e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request))); } });
